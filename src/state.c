6af87a9e92b16ba8f3a16f7adfa9a1fcd1f27173
rohan19a <arohan19a2@gmail.com>
2023-07-12 10:08:39 -0700

    task 2+3

--- src/state.c
+++ src/state.c
@@ -78,14 +78,22 @@ game_state_t* create_default_state() {
 
 /* Task 2 */
 void free_state(game_state_t* state) {
-  // TODO: Implement this function.
+  for (unsigned int i = 0; i < state->num_rows; i++) {
+    free(state->board[i]);
+  }
+  free(state->board);
 
-  return;
+  free(state->snakes);
+
+  free(state);
 }
+
 
 /* Task 3 */
 void print_board(game_state_t* state, FILE* fp) {
-  // TODO: Implement this function.
+  for (int i = 0; i < state->num_rows; i++) {
+    fprintf(fp, "%s\n", state->board[i]);
+  }
   return;
 }
 
